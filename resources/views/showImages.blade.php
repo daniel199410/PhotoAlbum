23900ff89a2f1dc6af112426c74a37a5e75508db
Daniel Cataño <dasniel19910@gmail.com>
2017-08-28 18:31:15 -0500

    Correcciones

--- resources/views/showImages.blade.php
+++ resources/views/showImages.blade.php
@@ -8,10 +8,10 @@
         <div class="row">
             @foreach($images as $image)
                 <div class="col-md-3">
-                    <a href="{{url('image/'.$nickname.'/'.$image->image_title)}}">
+                    <a href="{{url('image/'.$nickname.'/'.$image->title)}}">
                         <figure class="media-container">
                             <img src="{{ url('storage/'.$image->photo) }}" width="200" height="200">
-                            <figcaption>{{ $image->image_title }}</figcaption>
+                            <figcaption>{{ $image->title }}</figcaption>
                         </figure>
                     </a>
                 </div>
